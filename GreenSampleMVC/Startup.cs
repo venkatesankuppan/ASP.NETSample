8176283c7e61c094008e54fe7fece462505117e0
venkatesan kuppan <netvenkat@live.com>
2017-12-03 15:59:18 +0530

    Initial Commit

---
+++ GreenSampleMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GreenSampleMVC.Startup))]
+namespace GreenSampleMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
